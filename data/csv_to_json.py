b4c58c7738f68577a86b575d02dd4600c047d04a
Jesse Ponugoti <jesse.ponugoti@usask.ca>
2018-03-19 18:13:02 -0600

    create json of dataset based on location

---
+++ data/csv_to_json.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from collections import defaultdict
+import json
+
+df = pd.read_csv("dataset.csv", na_values=['x', '..'])
+df.dropna(how='any', inplace=True)
+
+# remove irrelevant columns from dataset
+df.drop(labels=["Vector", "Coordinate"], axis=1, inplace=True)
+df.reset_index(drop=True, inplace=True)
+
+# normalize headers
+df.rename(columns={"Ref_Date": "YEAR", "Value": "VALUE"}, inplace=True)
+
+# remove 'primary energy' and 'secondary energy' descriptors
+df.replace([", primary energy", ", secondary energy"], "", regex=True, inplace=True)
+
+df.to_csv("formatted.csv", index=False)
+
+# Make a Json object of all the data based on GEO
+data = defaultdict(lambda: defaultdict(lambda: defaultdict(dict)))
+
+for r in df.itertuples():
+    data[r.GEO][r.FUEL][r.SUPPLY][r.YEAR] = r.VALUE
+
+with open("dataset.json", mode='w') as f:
+    f.write(json.dumps(data, indent=2))
--- data/format_data.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("dataset.csv", na_values=['x', '..'])
-
-df.dropna(how='any', inplace=True)
-df.drop(labels=["Vector", "Coordinate"], axis=1, inplace=True)
-df.rename(columns={"Ref_Date": "YEAR", "Value": "VALUE"}, inplace=True)
-
-df.to_csv("formatted.csv", index=False)
